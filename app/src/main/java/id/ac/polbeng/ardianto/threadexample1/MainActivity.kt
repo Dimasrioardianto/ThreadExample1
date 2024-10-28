9960d55320e89432335cec099fa9e7f51e79a321
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-29 05:32:15 +0700

    First Commit

---
+++ app/src/main/java/id/ac/polbeng/ardianto/threadexample1/MainActivity.kt
@@ -0,0 +1,26 @@
+package id.ac.polbeng.ardianto.threadexample1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.ardianto.threadexample1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG ="MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartThread.setOnClickListener {
+            startThread()
+        }
+    }
+
+    private
+}
